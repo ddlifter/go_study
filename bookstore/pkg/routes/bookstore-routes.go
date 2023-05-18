37ffc4f9dfdbe01dccd1fb23ce457b8f9f7b44a4
ddlifter <132294753+ddlifter@users.noreply.github.com>
2023-05-18 22:50:19 +0300

    routes 1.1

--- bookstore/pkg/routes/bookstore-routes.go
+++ bookstore/pkg/routes/bookstore-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	contollers "github.com/ddlifter/go_study/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", contollers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", contollers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", contollers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", contollers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", contollers.DeleteBook).Methods("DELETE")
+}
