eb2ea64227b759abd08059c85d1795d973e35727
ddlifter <132294753+ddlifter@users.noreply.github.com>
2023-05-24 23:21:34 +0300

    final without testing

--- bookstore/pkg/controllers/book-controller.go
+++ bookstore/pkg/controllers/book-controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/ddlifter/go_study/pkg/models"
+	"github.com/ddlifter/go_study/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -29,6 +30,55 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	}
 	bookDetails, _ := models.GetBookById(ID)
 	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("--------")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var UpdateBook = &models.Book{}
+	utils.ParseBody(r, UpdateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("-----------")
+	}
+	booksDetails, db := models.GetBookById(ID)
+	if UpdateBook.Name != "" {
+		booksDetails.Name = UpdateBook.Name
+	}
+	if UpdateBook.Author != "" {
+		booksDetails.Author = UpdateBook.Author
+	}
+	if UpdateBook.Publication != "" {
+		booksDetails.Publication = UpdateBook.Publication
+	}
+	db.Save(&booksDetails)
+	res, _ := json.Marshal(booksDetails)
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
